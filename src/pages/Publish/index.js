776b587a611139c857c6ca49db1e2fa216318677
lrk <1196824608@qq.com>
2023-12-10 15:43:27 +0800

    限制图片最大上传数量

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -50,6 +50,17 @@ const Publish = () => {
     message.success('发布文章成功')
    
   }
+    // 上传图片
+  const [imageList, setImageList] = useState([])
+  const onUploadChange =(info) => { 
+    setImageList(info.fileList)
+  }
+  const [imageType, setImageType] = useState(0)
+
+  const onTypeChange = (e) => {
+    console.log(e)
+    setImageType(e.target.value)
+  }
     return (
       <div className="publish">
         <Card
@@ -64,7 +75,7 @@ const Publish = () => {
           <Form
             labelCol={{ span: 4 }}
             wrapperCol={{ span: 16 }}
-            initialValues={{ type: 1 }}
+            initialValues={{ type: 0 }}
             onFinish={onFinish}
           >
             <Form.Item
@@ -85,6 +96,30 @@ const Publish = () => {
                 )}  
               </Select>
             </Form.Item>
+            <Form.Item label="封面">
+  <Form.Item name="type">
+    <Radio.Group onChange={onTypeChange}>
+      <Radio value={1}>单图</Radio>
+      <Radio value={3}>三图</Radio>
+      <Radio value={0}>无图</Radio>
+    </Radio.Group>
+              </Form.Item>
+              { imageType > 0 && <Upload
+                name="image"
+                // 决定选择文件框的外观样式
+              listType="picture-card" 
+                showUploadList
+                action={'http://geek.itheima.net/v1_0/upload'}
+                onChange={onUploadChange}
+                maxCount={imageType}
+                multiple={imageType > 1}
+  >
+    <div style={{ marginTop: 8 }}>
+      <PlusOutlined />
+    </div>
+  </Upload> }
+             
+</Form.Item>
             <Form.Item
               label="内容"
               name="content"
